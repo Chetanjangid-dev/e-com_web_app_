a9ab953111be88d28a5c6f1c3b4180379a885d1f
Chetan jangid <jangidchetan.jpr@gmail.com>
2026-04-16 14:31:33 +0530

    Update products.routes.js

--- server/routes/products.routes.js
+++ server/routes/products.routes.js
@@ -2,26 +2,18 @@
 //  * routes/products.routes.js
 //  */
 
-// const router = require('express').Router();
-// const {
-//   getAllProducts,
-//   getCategories,
-//   getProductById,
-// } = require('../controllers/products.controller');
+const router = require('express').Router();
+const {
+  getAllProducts,
+  getCategories,
+   getProductById,
+ } = require('../controllers/products.controller');
 
-// // NOTE: /categories must come before /:id so Express doesn't treat "categories" as an id.
-// router.get('/categories', getCategories);
-// router.get('/',           getAllProducts);
-// router.get('/:id',        getProductById);
+ // NOTE: /categories must come before /:id so Express doesn't treat "categories" as an id.
+ router.get('/categories', getCategories);
+ router.get('/',           getAllProducts);
+ router.get('/:id',        getProductById);
 
-// module.exports = router;
+ module.exports = router;
 
-const express = require('express');
-const router = express.Router();
-const { getProducts, getProductById, addProduct } = require('../controllers/products.controller');
 
-router.get('/', getProducts);
-router.get('/:id', getProductById);
-router.post('/add', addProduct); // Ye line zaroori hai
-
-module.exports = router;
